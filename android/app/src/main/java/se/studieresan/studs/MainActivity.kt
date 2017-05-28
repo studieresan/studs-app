622eb09a3af529762d207035efbf3e5924911c0c
jakobivarsson <jakob.ivarsson94@gmail.com>
2017-05-28 15:59:11 +0200

    fetch locations from firebase and display on map

--- android/app/src/main/java/se/studieresan/studs/MainActivity.kt
+++ android/app/src/main/java/se/studieresan/studs/MainActivity.kt
@@ -9,25 +9,53 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.FirebaseDatabase
+import se.studieresan.studs.extensions.FirebaseAPI
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap?) {
         Log.d("MAIN", "map ready")
-        val timesSquare = LatLng(40.758899, -73.9873197)
-        googleMap?.apply {
-            addMarker(MarkerOptions().position(timesSquare).title("Party"))
-            animateCamera(CameraUpdateFactory.newLatLngZoom(timesSquare, 15F))
-        }
+        var first = true
+        locationListener = FirebaseAPI.createChildEventListener({ snap ->
+            val lat = snap.child("lat").value as Double
+            val lng = snap.child("lng").value as Double
+            val msg = snap.child("message").value as String
+            val location = LatLng(lat, lng)
+            googleMap?.apply {
+                addMarker(MarkerOptions().position(location).title(msg))
+                if (first) {
+                    animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15F))
+                    first = false
+                }
+            }
+        })
+        locations.addChildEventListener(locationListener)
     }
+
+    var locationListener: ChildEventListener? = null
 
     val map by lazy {
         supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
     }
 
+    val locations by lazy {
+        val db = FirebaseDatabase.getInstance()
+        db.getReference("locations").limitToLast(20)
+//                .orderByKey()
+//                .startAt(System.currentTimeMillis().toDouble()/1000-3600)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         map.getMapAsync(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locationListener?.let {
+            locations.removeEventListener(it)
+        }
     }
 }
---
+++ android/app/src/main/java/se/studieresan/studs/extensions/FirebaseAPI.kt
@@ -0,0 +1,65 @@
+package se.studieresan.studs.extensions
+
+import android.util.Log
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+
+object FirebaseAPI {
+
+    private val TAG = FirebaseAPI::class.java.simpleName
+
+    fun createValueEventListener(
+            onData: (DataSnapshot) -> Unit,
+            onCancelled: (DatabaseError) -> Unit = {}
+    ): ValueEventListener {
+        return object: ValueEventListener {
+            override fun onDataChange(snap: DataSnapshot?) {
+                snap ?: return
+                onData(snap)
+            }
+
+            override fun onCancelled(error: DatabaseError?) {
+                Log.e(TAG, "Firebase Error: $error")
+                onCancelled(error)
+            }
+        }
+    }
+
+    fun createChildEventListener(
+            onAdded: (DataSnapshot) -> Unit,
+            onRemoved: (DataSnapshot) -> Unit = {},
+            onChanged: (DataSnapshot) -> Unit = {},
+            onMoved: (DataSnapshot) -> Unit = {},
+            onCancelled: (DatabaseError) -> Unit = {}
+    ): ChildEventListener {
+        return object: ChildEventListener {
+            override fun onCancelled(error: DatabaseError?) {
+                error ?: return
+                onCancelled(error)
+            }
+
+            override fun onChildMoved(snap: DataSnapshot?, prevName: String?) {
+                snap ?: return
+                onMoved(snap)
+            }
+
+            override fun onChildChanged(snap: DataSnapshot?, prevName: String?) {
+                snap ?: return
+                onChanged(snap)
+            }
+
+            override fun onChildAdded(snap: DataSnapshot?, prevName: String?) {
+                snap ?: return
+                onAdded(snap)
+            }
+
+            override fun onChildRemoved(snap: DataSnapshot?) {
+                snap ?: return
+                onRemoved(snap)
+            }
+        }
+    }
+}
+
