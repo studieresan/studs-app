c001a24b03d7416a4fd38922315e4e129d559fd3
jakobivarsson <jakob.ivarsson94@gmail.com>
2017-05-31 21:38:43 +0200

    add function that sends a fcm notification to user subscribed to topic 'locations' when somebody shares a location

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,4 +1,5 @@
 var functions = require('firebase-functions');
+var admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,24 @@ var functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+var topic = 'locations';
+
+exports.notifyLocation = functions.database.ref('/locations')
+  .onWrite(event => {
+    if (event.data.previous.exists()) {
+      return;
+    }
+    var key = event.data.key;
+    var payload = {
+      notification: {
+        title: 'Somebody shared a location',
+        body: 'Tap to view',
+        clickAction: 'location'
+      },
+      data: {
+        locationKey: key
+      }
+    };
+    return admin.messaging().sendToTopic(topic, payload);
+  });
