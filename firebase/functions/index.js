d8f9482aab9407f239cc6ccdc94055b6c9365b4d
jakobivarsson <jakob.ivarsson94@gmail.com>
2017-06-12 17:17:58 +0200

    update firebase

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,6 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,21 +11,26 @@ var admin = require('firebase-admin');
 
 var topic = 'locations';
 
-exports.notifyLocation = functions.database.ref('/locations')
+exports.notifyLocation = functions.database.ref('/locations/{key}')
   .onWrite(event => {
     if (event.data.previous.exists()) {
       return;
     }
     var key = event.data.key;
+    var user = event.data.val().user;
     var payload = {
       notification: {
         title: 'Somebody shared a location',
-        body: 'Tap to view',
-        clickAction: 'location'
+        body: 'Tap to view'
       },
       data: {
-        locationKey: key
+        locationKey: key,
+        user: user
       }
     };
-    return admin.messaging().sendToTopic(topic, payload);
+    var options = {
+      priority: 'high',
+      timeToLive: 60*60
+    };
+    return admin.messaging().sendToTopic(topic, payload, options);
   });
